834180754b9115a79b19564a4452219b59de686c
hammam995 <hammamzahran@gmail.com>
2023-01-30 12:49:31 +0300

    finishing IsWalking condition

--- Test/Assets/Script/Unit.cs
+++ Test/Assets/Script/Unit.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator; // top controll the animatior conditions
+
     private Vector3 targetPosition; // the goal
    
     private void Update()
     {
         float stoppingDistance = .1f; // the condition to put it so the character stop and not keep moving
-        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance) // if the distance between the character and the target is largest than the stopping distance then do
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance) // if the distance between the character and the target is largest than the stopping distance then do movement
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized; // direction == target - character  position
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime; // to move the character == the direction * movespeed *  Time.deltaTime
+            
+            unitAnimator.SetBool("IsWalking", true); // Animation movement condition will be true if we aren't reaching to the stopping distsnce
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking", false); // Animation movement condition will be false if we are reaching to the stopping distsnce
         }
         if (Input.GetMouseButtonDown(0))
         {
